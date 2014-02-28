8cfd924b38bd0bbfc713bd2c57d99d887d24ee5a
zhswatson <zhswatson@gmail.com>
2014-02-28 10:24:28 -0500

    changed file readin from line to char

--- hash/compare/readinfile.java
+++ hash/compare/readinfile.java
@@ -7,17 +7,26 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 
 public class readinfile {
-	public static ArrayList readin(String filename) throws IOException, ClassNotFoundException, NoSuchAlgorithmException 
+	public static ArrayList<String> readin(String filename) throws IOException, ClassNotFoundException, NoSuchAlgorithmException 
 	{
 		String str;
-		ArrayList result = new ArrayList();
+		char[] cbuf = new char[8];
+		ArrayList<String> result = new ArrayList<String>();
+		@SuppressWarnings("resource")
 		BufferedReader in = new BufferedReader( new InputStreamReader(new FileInputStream(filename)));
-		while ((str=in.readLine())!=null)
+/*		while ((str=in.readLine())!=null)
 		{
 			result.add(hashcodegenerator.makehash(str));
+		}
+*/		while (in.read(cbuf, 0, 5) != -1) {
+			str = new String(cbuf);
+			result.add(hashcodegenerator.makehash(str));
+			System.out.println(str);
+			Arrays.fill(cbuf, ' ');
 		}
 		return result;
 	}
--- hash/compare/tester.java
+++ hash/compare/tester.java
@@ -7,15 +7,15 @@ public class tester {
 	public static void main(String [] args) throws NoSuchAlgorithmException, ClassNotFoundException, IOException
 	{
 		
-		ArrayList ori = readinfile.readin("plain.txt");
+		ArrayList<String> ori = readinfile.readin("plain.txt");
 		//System.out.println(ori.get(0));
-		ArrayList neww = readinfile.readin("change.txt");
+		ArrayList<String> neww = readinfile.readin("change.txt");
 		//System.out.println(neww.get(0));
 		//System.out.println(ori.get(0).equals(neww.get(0)));
 		compare(ori,neww);
 		
 	}
-	public static boolean compare(ArrayList a, ArrayList b)
+	public static boolean compare(ArrayList<String> a, ArrayList<String> b)
 	{
 		if (a.size()!=b.size())
 			return false;
