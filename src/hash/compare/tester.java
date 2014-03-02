716728d3f0336044071328a556232c3b6ae8d14d
sunrisebay <mpan@bu.edu>
2014-03-01 22:46:39 -0500

    work for one characer differ

--- src/hash/compare/tester.java
+++ src/hash/compare/tester.java
@@ -8,46 +8,139 @@ public class tester {
 	{
 		
 		ArrayList<String> ori = readinfile.readin("plain.txt");
-		//System.out.println(ori.get(0));
-		ArrayList<String> neww = readinfile.readin("change.txt");
-		//System.out.println(neww.get(0));
-		//System.out.println(ori.get(0).equals(neww.get(0)));
-		compare(ori,neww);
 		
-	}
-	public static boolean compare(ArrayList<String> a, ArrayList<String> b)
-	{
-		byte [] aa = null;
-		byte [] bb = null;
-		if (a.size()!=b.size())
-			return false;
-		else
+		ArrayList<String> neww = readinfile.readin("change.txt");
+		
+		if (initialcheck(ori,neww)==false)
 		{
-			for (int ii = 0 ; ii<a.size() ; ii++)
+			if ( diff.get(0).length() == diff.get(1).length())
+				checkchange(diff.get(0),diff.get(1));
+			else
 			{
-				if ((a.get(ii).equals(b.get(ii)))==false)
-				{
-					//System.out.println(a.get(ii));
-					//System.out.println(b.get(ii));
-					aa = readinfile.readinbyte(a.get(ii));
-					bb = readinfile.readinbyte(b.get(ii));
-				}
-					
+				if (diff.get(0).length() < diff.get(1).length())
+					checkinsert(diff.get(0),diff.get(1));
+				else
+					checkdelete(diff.get(0),diff.get(1));
+			}
+		}
+		//log(ori.get(0));
+	}
+	
+	private static boolean initialcheck(ArrayList ori, ArrayList neww) throws NoSuchAlgorithmException
+	{
+		if (ori.size()!=neww.size())
+		{
+			log("diff size");
+			return false;
+		}
+		for (int ii = 0 ; ii < ori.size() ; ii++)
+		{
+			if(hashcodegenerator.makehash(ori.get(ii).toString()).equals(hashcodegenerator.makehash(neww.get(ii).toString())))
+			{
+			
+			}
+			else
+			{
+				diff.add(ori.get(ii).toString());
+				diff.add(neww.get(ii).toString());
+				log(ori.get(ii).toString());
+				log(neww.get(ii).toString());
+				return false;
 			}
 			
-			//System.out.println(aa.length);
-			//System.out.println(bb.length);
-			
 		}
-		for (int ii = 0 ; ii<Math.min(aa.length, bb.length) ; ii++)
-		{
-			if (aa[ii]!=bb[ii])
-				System.out.println(aa[ii]);
-			
-				
-		}
-		
-		return false;
+		return true;
 	}
+	
+		
+	private static void checkinsert(String a, String b)
+	{
+		int ai = 0 ;
+		int bi = 0;
+		
+		/*for (int ii = 0 ; ii < a.length()-BYTE ; ii++)
+		{
+			if (a.substring(ii, ii+BYTE).equals(b.substring(ii, ii+BYTE)))
+			{
+				
+			}
+			else
+			{
+				log("diff point : " +ii);
+				
+				
+			}
+		}*/
+		
+		while(ai<a.length()-BYTE && bi<b.length()-BYTE)
+		{
+			if (a.substring(ai, ai+BYTE).equals(b.substring(bi, bi+BYTE)))
+			{
+				
+			}
+			else
+			{
+				log("diff point : " +ai + " " + bi);
+				ai = ai+BYTE;
+				bi=bi+BYTE+1;
+				
+			}
+			ai++;
+			bi++;
+		}
+		log("diff point : " + bi);
+		
+	}
+	
+	
+	private static void checkdelete(String a, String b)
+	{
+
+		int ai = 0 ;
+		int bi = 0;
+		while(ai<a.length()-BYTE && bi<b.length()-BYTE)
+		{
+			if (a.substring(ai, ai+BYTE).equals(b.substring(bi, bi+BYTE)))
+			{
+				
+			}
+			else
+			{
+				log("diff point : " +ai + " " + bi);
+				ai = ai+BYTE+1;
+				bi=bi+BYTE;
+				
+			}
+			ai++;
+			bi++;
+		}
+		log("diff point : " + bi);
+	}
+	
+	private static void checkchange(String a, String b)
+	{
+		for (int ii = 0 ; ii < a.length()-BYTE ; ii++)
+		{
+			if (a.substring(ii, ii+BYTE).equals(b.substring(ii, ii+BYTE)))
+			{
+				
+			}
+			else
+			{
+				log("diff point : " +ii);
+				
+			}
+		}
+	}
+	
+	private static void log(String a)
+	{
+		System.out.println(a);
+	}
+	
+	private static int offsetinsert=0;
+	private static int offsetdelete=0;
+	private final static int BYTE = 5;
+	private static ArrayList<String> diff = new ArrayList<String>();
 
 }
